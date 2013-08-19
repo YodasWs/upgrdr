a31e63397f2454ac94ccf583555a42b51d7ac8fb
Samuel Grundman <samuel@yodas.ws>
2013-08-19 19:53:35 -0400

    Polyfill HTML5 History API
    Work to polyfill HTML5's History API with onhashchange (not yet ready
    for IE8)

---
+++ history.js
@@ -0,0 +1,40 @@
+﻿if (typeof history.pushState !== 'function') {
+	history.states = [];
+	history.pushState = function(state, title, url) {
+		var search = {};
+		var query = location.search || '?';
+		var vars = query.replace(/^\?/,'').split('&');
+		for (i in vars) {
+			key = decodeURIComponent(vars[i].substr(0, vars[i].indexOf('=')));
+			search[key] = decodeURIComponent(vars[i].substr(vars[i].indexOf('=')+1));
+		}
+		search.state = encodeURIComponent(JSON.stringify(state))
+		search.url = encodeURIComponent(url);
+// TODO: Use url as hash like ajaxify, https://gist.github.com/balupton/858093
+		var hash = parseInt(location.hash.replace(/^[\/#]+/,''), 10) + 1 || 1;
+		history.states[hash] = search;
+		location.hash = hash;
+	}
+// TODO: history.replaceState
+	history.replaceState = function(state, title, url) {
+	}
+	window.onhashchange = function() {
+		history.state = history.states[parseInt(location.hash.substr(1), 10)];
+		// Create popstate Event, http://stackoverflow.com/questions/2490825/how-to-trigger-event-in-javascript
+		var event;
+		if (document.createEvent) {
+			event = document.createEvent("HTMLEvents");
+			event.initEvent("popstate", true, true);
+		} else {
+			event = document.createEventObject();
+			event.eventType = "popstate";
+		}
+		event.eventName = 'popstate';
+		event.memo = {};
+		if (document.createEvent) {
+			window.dispatchEvent(event);
+		} else {
+			window.fireEvent("on" + event.eventType, event);
+		}
+	}
+}
