441a053ba5f06613913d2c5da5f159309f0b1f59
Samuel Grundman <samuel@yodas.ws>
2013-08-19 19:48:22 -0400

    Upgrade Array
    Add Array.indexOf and Array.isArray (primarily for IE8)

---
+++ array.js
@@ -0,0 +1,13 @@
+﻿// JavaScript v1.6
+if (typeof (new Array).indexOf !== 'function')
+Array.prototype.indexOf = function(val) {
+	for (var i=0; i<this.length; i++)
+		if (this[i] === val) return i;
+	return -1;
+}
+// JavaScript v1.8.5
+if(!Array.isArray) {
+	Array.isArray = function (vArg) {
+		return Object.prototype.toString.call(vArg) === "[object Array]";
+	};
+}
