4a84f976a13941bad963a9bad37ae324ba65f335
Samuel Grundman <samuel@yodas.ws>
2013-08-24 11:08:45 -0400

    Scroll Navigation
    Single-page navigation using scroll! Will set active region when user is
    reading it not at top of window frame :) Includes min and max scroll
    time.

--- batch.js
+++ batch.js
@@ -3,12 +3,30 @@ Array.prototype.indexOf = function(val) {
 	for (var i=0; i<this.length; i++)
 		if (this[i] === val) return i;
 	return -1;
-}
+};
 if (!window.upgrdr) upgrdr = {
+	_options:{ // Default Options
+	},
+	// Let User Override Default Options
+	options:function() {
+		if (arguments.length) {
+			arguments = Array.prototype.slice.call(arguments);
+			var object = arguments.shift();
+			if (!upgrdr._options[object])
+				upgrdr._options[object] = {};
+			for (i in arguments) {
+				if (typeof arguments[i] == 'object')
+					for (j in arguments[i])
+						upgrdr._options[object][j] = arguments[i][j];
+			}
+		}
+		// Return for chaining, e.g. upgrdr.options().scroll.easing
+		return upgrdr._options;
+	},
 	added:[],
 	history:function() {
 		if (typeof history.pushState !== 'function')
-			document.write('<script src="/src/history.js" type="text/javascript"><\/script>');
+			document.write('<script src="src/history.js" type="text/javascript"><\/script>');
 	},
 	add:function() {
 		for(i=0; i<arguments.length; i++) {
@@ -20,13 +38,13 @@ if (!window.upgrdr) upgrdr = {
 				continue;
 			}
 			// Download jQuery
-			if (!window.jQuery && this.added.indexOf('jQuery') == -1 && ['select'].indexOf(arguments[i]) > -1) {
+			if (!window.jQuery && this.added.indexOf('jQuery') == -1 && ['select','scroll'].indexOf(arguments[i]) > -1) {
 				document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"><\/script>');
 				this.added.push('jQuery');
 			}
 			// Download External File
-			if (['select','array','string','batch'].indexOf(arguments[i]) > -1 && this.added.indexOf(arguments[i]) == -1) {
-				document.write('<script src="/src/' + arguments[i] + '.js" type="text/javascript"><\/script>');
+			if (['select','array','string','scroll'].indexOf(arguments[i]) > -1 && this.added.indexOf(arguments[i]) == -1) {
+				document.write('<script src="src/' + arguments[i] + '.js" type="text/javascript"><\/script>');
 				this.added.push(arguments[i]);
 				continue;
 			}
---
+++ src/navScroll.js
@@ -0,0 +1,74 @@
+﻿// For beautiful single-page navigation
+upgrdr.add('jQuery');
+if (!upgrdr._options) upgrdr._options = {};
+if (!upgrdr._options.navScroll) upgrdr._options.navScroll = {
+	easing:'easeInOutSine',
+	maxTime:1000,
+	minTime:400,
+	margin:'40%',
+	speed:800,
+	unit:'ms'
+};
+$(document).ready(function() {
+	$('body > nav > a[href^="#"]').bind('click.nav', function() {
+		if ($(this).is('.active')) return false;
+		var speed, pos = $($(this).attr('href')).offset().top;
+		if ($(this).attr('href') == '#top') pos = 0;
+		// Calculate Animation Runtime
+		switch(upgrdr.options().navScroll.unit) {
+		case 'ms': // milliseconds
+			speed = upgrdr.options().navScroll.speed;
+			break;
+		case 'sec': case 's': // seconds
+			speed = upgrdr.options().navScroll.speed * 1000;
+			break;
+		case 'ms/hp': // milliseconds per 100 pixels
+			speed = upgrdr.options().navScroll.speed * Math.abs($(document).scrollTop()- pos) / 100;
+			break;
+		case 'ms/kp': // milliseconds per 1000 pixels
+			speed = upgrdr.options().navScroll.speed * Math.abs($(document).scrollTop()- pos) / 1000;
+			break;
+		}
+		// Constrain animation runtime (default 400 < x < 1000)
+		speed = Math.min(Math.max(speed, upgrdr.options().navScroll.minTime), upgrdr.options().navScroll.maxTime);
+		// Scroll!
+		$('body').stop(true, false).animate({
+			scrollTop:pos
+		}, speed, upgrdr.options().navScroll.easing, function() {
+		});
+		return false;
+	});
+	$(window).scroll(function() {
+		var minScroll = parseInt($('body > nav').css('top'), 10) || 0;
+		// Position Main Menu
+		if ($('body').scrollTop() > minScroll) {
+			$('body > nav').css({
+				position:'fixed',top:'0px'
+			});
+		} else {
+			$('body > nav').css({
+				position:'',top:''
+			});
+		}
+		// Active Current Nav Link
+		var lastY = 0, navPos = -1;
+		$('body > nav > a[href^="#"]').each(function(i) {
+			lastY = $($(this).attr('href')).offset().top;
+			if (typeof upgrdr.options().navScroll.margin == 'number')
+				lastY -= upgrdr.options().navScroll.margin;
+			else if (/\d+%/.test(upgrdr.options().navScroll.margin))
+				lastY -= parseInt(upgrdr.options().navScroll.margin, 10) * $(window).height() / 100;
+			if ($('body').scrollTop() < lastY) {
+				navPos = Math.max(i-1, 0);
+				return false;
+			}
+		}).removeClass('active');
+		if (navPos < 0) navPos = $('body > nav > a[href^="#"]').length-1;
+		if ($($('body > nav > a[href^="#"]').get(navPos)).attr('href') != '#top')
+			$($('body > nav > a[href^="#"]').get(navPos)).addClass('active');
+	}).keyup(function(event) {
+		if (event.keyCode == 27) // Escape
+			// Stop Automatic Scrolling
+			$('body').stop(true, false);
+	});
+});
