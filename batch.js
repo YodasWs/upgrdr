3fe53ed0a7cfc128a9da2b0806202cdebe9bb2fc
Samuel Grundman <samuel@yodas.ws>
2013-08-24 10:26:39 -0400

    Rearrange Files
    Place JS files in src directory for easy reference, separate from
    example pages. Also update paths to correct files.

--- batch.js
+++ batch.js
@@ -1,5 +1,4 @@
-﻿/*@cc_on @*/
-if (typeof (new Array).indexOf !== 'function')
+﻿if (typeof (new Array).indexOf !== 'function')
 Array.prototype.indexOf = function(val) {
 	for (var i=0; i<this.length; i++)
 		if (this[i] === val) return i;
@@ -9,22 +8,9 @@ if (!window.upgrdr) upgrdr = {
 	added:[],
 	history:function() {
 		if (typeof history.pushState !== 'function')
-			document.write('<script src="/upgrdr/history.php" type="text/javascript"><\/script>');
+			document.write('<script src="/src/history.js" type="text/javascript"><\/script>');
 	},
 	add:function() {
-/*@if (@_jscript_version < 9) // IE8 didn't like for in arguments ?!
-		for(i=0; i<arguments.length; i++) {
-			if (typeof this[arguments[i]] === 'function') {
-				if (this.added.indexOf(arguments[i]) > -1) continue;
-				this[arguments[i]]();
-				this.added.push(arguments[i]);
-			}
-		}
-	@else @*/
-//		for(i in arguments) {
-//		}
-	/*@end
-@*/
 		for(i=0; i<arguments.length; i++) {
 			// Run upgrdr Function
 			if (typeof this[arguments[i]] === 'function') {
@@ -40,7 +26,7 @@ if (!window.upgrdr) upgrdr = {
 			}
 			// Download External File
 			if (['select','array','string','batch'].indexOf(arguments[i]) > -1 && this.added.indexOf(arguments[i]) == -1) {
-				document.write('<script src="/upgrdr/' + arguments[i] + '.php" type="text/javascript"><\/script>');
+				document.write('<script src="/src/' + arguments[i] + '.js" type="text/javascript"><\/script>');
 				this.added.push(arguments[i]);
 				continue;
 			}
