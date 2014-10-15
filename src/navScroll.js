be255dc569623d39da6d875e54648cb9d0f7da2c
Samuel Grundman <samuel@yodas.ws>
2014-10-14 21:39:59 -0400

    Beautiful scrolling page with in-page nav
    Remove useless semicolons
    Improved performance and animation.
    Using jQuery animation queue
    And using update upgrdr/batch.js for easier/faster option object access

--- src/navScroll.js
+++ src/navScroll.js
@@ -1,81 +1,103 @@
-﻿// For beautiful single-page navigation
+// For beautiful single-page navigation
 
 // jQuery Required :-\
-upgrdr.add('jQuery');
+upgrdr.add('jQuery')
+upgrdr.add('math')
 
 // Set Default Options
-if (!upgrdr._options) upgrdr._options = {};
-if (!upgrdr._options.navScroll) upgrdr._options.navScroll = {};
-(function(defaults) {
+;(function(defaults) {
+	var options = upgrdr.options('navScroll')
 	for (i in defaults) {
 		// Do not override user-entered options, if any
-		if (!upgrdr._options.navScroll[i])
-			upgrdr._options.navScroll[i] = defaults[i];
+		if (!options[i])
+			upgrdr.options('navScroll', i, defaults[i])
 	}
-})({easing:'easeInOutSine',maxTime:1000,minTime:400,margin:'40%',speed:800,unit:'ms'});
+})({easing:'easeInOutSine',maxTime:1000,minTime:400,margin:'40%',speed:800,unit:'ms'})
 
-$(document).ready(function() {
+upgrdr.jqReady(function() { $(document).ready(function() {
 	// On Navigation, Scroll into View
-	$('body > nav > a[href^="#"]').bind('click.nav', function() {
-		if ($(this).is('.active')) return false;
-		var speed, pos = $($(this).attr('href')).offset().top;
-		if ($(this).attr('href') == '#top') pos = 0;
+	$('body').on('click.nav', 'a[href^="#"]', function() {
+		if ($(this).is('.active')) return false
+		var speed, scrollStart = NaN, pos = $($(this).attr('href')).offset().top, scrollDiff = pos - $(document).scrollTop(),
+			options = upgrdr.options('navScroll')
+		if ($(this).attr('href') == '#top') pos = 0
 		// Calculate Animation Runtime
-		switch(upgrdr.options().navScroll.unit) {
+		switch(options.unit) {
 		case 'ms': // milliseconds
-			speed = upgrdr.options().navScroll.speed;
-			break;
+			speed = options.speed
+			break
 		case 'sec': case 's': // seconds
-			speed = upgrdr.options().navScroll.speed * 1000;
-			break;
+			speed = options.speed * 1000
+			break
 		case 'ms/hp': // milliseconds per 100 pixels
-			speed = upgrdr.options().navScroll.speed * Math.abs($(document).scrollTop()- pos) / 100;
-			break;
+			speed = options.speed * Math.abs(scrollDiff) / 100
+			scrollStart = options.maxTime / options.speed * 100
+			break
 		case 'ms/kp': // milliseconds per 1000 pixels
-			speed = upgrdr.options().navScroll.speed * Math.abs($(document).scrollTop()- pos) / 1000;
-			break;
+			speed = options.speed * Math.abs(scrollDiff) / 1000
+			scrollStart = options.maxTime / options.speed * 1000
+			break
 		}
-		// Constrain animation runtime (default 400 < x < 1000)
-		speed = Math.min(Math.max(speed, upgrdr.options().navScroll.minTime), upgrdr.options().navScroll.maxTime);
+		// Set Minimum Animation Runtime
+		speed = Math.max(speed, options.minTime)
+		// If speed > maxTime, jump to where animation will take maxTime then animate scroll
+		if (speed > options.maxTime) {
+			if (isNaN(scrollStart)) speed = options.maxTime
+			else {
+				$('html, body').stop('scroll', true, false).animate({
+					scrollTop: pos - Math.sign(scrollDiff) * scrollStart
+				}, {
+					duration: 0,
+					queue: 'scroll'
+				}).dequeue('scroll')
+			}
+		}
+
 		// Scroll!
-		$('body').stop(true, false).animate({
+		$('body').stop('scroll', true, false).animate({
 			scrollTop:pos
-		}, speed, upgrdr.options().navScroll.easing, function() {
-		});
-		return false;
-	});
-	$(window).scroll(function() {
-		var minScroll = parseInt($('body > nav').css('top'), 10) || 0;
+		}, {
+			duration: speed,
+			queue: 'scroll',
+			easing: options.easing
+		}).dequeue('scroll')
+		return false
+	})
+	var moveMenu = function() {
+		var minScroll = parseInt($('body > nav').css('top'), 10) || 0,
+			lastY = 0, navPos = -1, navEle
 		// Position Main Menu
 		if ($('body').scrollTop() > minScroll) {
 			$('body > nav').css({
 				position:'fixed',top:'0px'
-			});
+			})
 		} else {
 			$('body > nav').css({
 				position:'',top:''
-			});
+			})
 		}
 		// Active Current Nav Link
-		var lastY = 0, navPos = -1;
 		$('body > nav > a[href^="#"]').each(function(i) {
-			lastY = $($(this).attr('href')).offset().top;
+			var options = upgrdr.options('navScroll'),
+			   lastY = $($(this).attr('href')).offset().top
 			// Set offset margin for distance active section is below scrollTop
-			if (typeof upgrdr.options().navScroll.margin == 'number')
-				lastY -= upgrdr.options().navScroll.margin;
-			else if (/\d+%/.test(upgrdr.options().navScroll.margin))
-				lastY -= parseInt(upgrdr.options().navScroll.margin, 10) * $(window).height() / 100;
+			if (typeof options.margin == 'number')
+				lastY -= options.margin
+			else if (/\d+%/.test(options.margin))
+				lastY -= parseInt(options.margin, 10) * $(window).height() / 100
 			if ($('body').scrollTop() < lastY) {
-				navPos = Math.max(i-1, 0);
-				return false;
+				navPos = Math.max(i-1, 0)
+				return false
 			}
-		}).removeClass('active');
-		if (navPos < 0) navPos = $('body > nav > a[href^="#"]').length-1;
-		if ($($('body > nav > a[href^="#"]').get(navPos)).attr('href') != '#top')
-			$($('body > nav > a[href^="#"]').get(navPos)).addClass('active');
-	}).keyup(function(event) {
+		}).removeClass('active')
+		if (navPos < 0) navPos = $('body > nav > a[href^="#"]').length-1
+		navEle = $($('body > nav > a[href^="#"]').get(navPos))
+		if (navEle.attr('href') != '#top')
+			navEle.addClass('active')
+	}
+	$(window).ready(moveMenu).scroll(moveMenu).keyup(function(event) {
 		if (event.keyCode == 27) // Escape
 			// Stop Automatic Scrolling
-			$('body').stop(true, false);
-	});
-});
+			$('body').stop('scroll', true, false)
+	})
+})})
